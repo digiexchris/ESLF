d71be9f19c093aea9daa8362c51f7b860ca96901
Chris Chatelain <chris@digiex.ca>
2024-02-09 21:37:55 -0600

    Set CPP version to 20

--- include/State/MessageBus/Router.hpp
+++ include/State/MessageBus/Router.hpp
@@ -16,16 +16,19 @@ template <typename TDerived, typename... Messages>
 class Router : public etl::message_router<TDerived, Messages...>
 {
 public:
-    virtual void receive(const etl::imessage& msg_);
+    virtual void receive(const etl::imessage& msg_) = 0;
 
 protected:
     
 };
-  template <typename TDerived, size_t QueueSize, typename... Messages>
+
+
+
+template <typename TDerived, size_t QueueSize, typename... Messages>
 class QueuedRouter : public Router<TDerived, Messages...>
 {
 public:
-
+    virtual void receive(const etl::imessage& msg_) override;
     //optional method to process the queue
     //some routers are simple enough to not need a queue so this is optional
     virtual void process_queue();
--- src/State/MessageBus/Router.cpp
+++ src/State/MessageBus/Router.cpp
@@ -12,40 +12,53 @@ namespace MessageBus
   // //***************************************************************************
   // // The overridden virtual receive function.
   //***************************************************************************
-template <typename TDerived, typename... Messages>
-class Router<TDerived, Messages...>::receive(const etl::imessage& msg_)
+// template <typename TDerived, typename... Messages>
+// Router<TDerived, Messages...>::receive(const etl::imessage& msg_)
+// {
+//   if (accepts(msg_))
+//   {
+//     ELSF_LOG_INFO("Received message %d\n", msg_.get_message_id());
+//   }
+//   else
+//   {
+//     ELSF_LOG_INFO("Message %d not accepted\n", msg_.get_message_id());
+//   }
+}
+
+//***************************************************************************
+// The method to call to handle any queued messages.
+//***************************************************************************
+template <typename TDerived, size_t QueueSize, typename... Messages>
+void QueuedRouter<TDerived, QueueSize, Messages...>::process_queue()
+{
+  while (!queue.empty())
   {
-    if (accepts(msg_))
-    {
-      // Place in queue.
-      queue.emplace(msg_);
-      ELSF_LOG_INFO("Queueing message %d\n", msg_.get_message_id());
-    }
-    else
-    {
-      ELSF_LOG_INFO("Message %d not accepted\n", msg_.get_message_id());
-    }
-  }
+    RouterMessagePacket<Messages...>& packet = queue.front();
+    etl::imessage& msg = packet.get();
+    ELSF_LOG_INFO("Processing message %d\n", msg.get_message_id());
 
-  //***************************************************************************
-  // The method to call to handle any queued messages.
-  //***************************************************************************
-  template <typename TDerived, size_t QueueSize, typename... Messages>
-  void QueuedRouter<TDerived, QueueSize, Messages...>::process_queue()
+    // Call the base class's receive function.
+    // This will route it to the correct 'on_event' handler.
+    //TODO figure that out. Maybe the FSM extends this class. etl::fsm::receive(msg);
+
+    queue.pop();
+  }
+}
+
+template <typename TDerived, size_t QueueSize, typename... Messages>
+void QueuedRouter<TDerived, QueueSize, Messages...>::receive(const etl::imessage& msg_)
+{
+  if (accepts(msg_))
   {
-    // while (!queue.empty())
-    // {
-    //   RouterMessagePacket<Messages...>& packet = queue.front();
-    //   etl::imessage& msg = packet.get();
-    //   ELSF_LOG_INFO("Processing message %d\n", msg.get_message_id());
-
-    //   // Call the base class's receive function.
-    //   // This will route it to the correct 'on_event' handler.
-    //   etl::fsm::receive(msg);
-
-    //   queue.pop();
-    // }
+    // Place in queue.
+    queue.emplace(msg_);
+    ELSF_LOG_INFO("Received message %d\n", msg_.get_message_id());
   }
+  else
+  {
+    ELSF_LOG_INFO("Message %d not accepted\n", msg_.get_message_id());
+  }
+}
 
 
 } // namespace MessageBus
